0536447e5c1841cd7dc892272cdc424b251313cb
Jason Swift <anemele@outlook.com>
2023-10-05 23:04:30 +0800

    update:
    add a cli to print help message if no arg passed.

--- src/bin/file.rs
+++ src/bin/file.rs
@@ -1,10 +1,23 @@
 use filetype::match_all;
 use glob::glob;
 use std::env::{self};
+use std::path::MAIN_SEPARATOR_STR;
 
 fn main() {
     let mut args = env::args();
-    let _ = args.next(); // The program itself
+    let prog = args.next().unwrap();
+    let prog = match prog.rsplit_once(MAIN_SEPARATOR_STR) {
+        Some(p) => p.1,
+        None => prog.as_str(),
+    }; // The program itself
+
+    if args.len() == 0 {
+        println!(
+            "Determine file type via magic numbers, wildcards support.\nUsage: {} <file> [file ...] ",
+            prog
+        );
+        return;
+    }
 
     for arg in args {
         // println!("{arg}");
