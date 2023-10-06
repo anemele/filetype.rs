e6dee68e9f0397a522baf9a5a1f4ccfd47986271
Jason Swift <anemele@outlook.com>
2023-10-06 23:10:12 +0800

    bugfix: skip if permission denied

--- src/utils.rs
+++ src/utils.rs
@@ -4,12 +4,18 @@ use std::{fs::File, io::Read};
 pub fn get_signature_bytes(path: &Path, mut buffer: &mut [u8]) -> bool {
     let mut file = match File::open(path) {
         Ok(file) => file,
-        Err(e) => panic!("Unable to open file {}: {}", path.display(), e),
+        Err(e) => {
+            eprintln!("Unable to open file {}: {}", path.display(), e);
+            return false;
+        }
     };
 
     // let mut buffer = [0_u8; NUM_SIGNATURE_BYTES];
     match file.read(&mut buffer) {
         Ok(_) => true,
-        Err(e) => panic!("Unable to read file {}: {}", path.display(), e),
+        Err(e) => {
+            eprintln!("Unable to read file {}: {}", path.display(), e);
+            return false;
+        }
     }
 }
